501d37e7369697d3fad172a36370cbf05ad49d6a
Yoga Pratama <56101537+pratamayp@users.noreply.github.com>
2019-12-12 09:04:06 +0700

    Revisi

--- SmallProject.java
+++ SmallProject.java
@@ -1,83 +1,8 @@
 import java.util.Scanner;
-
 public class SmallProject {
 
-	static Scanner in = new Scanner(System.in);
-	public static void main(String[] args) {
-		
-		print("==RAPORT==\n");
-
-		String nama, nim;
-        int nilaiUTS, nilaiUAS;
-        int[]nilai = new int[4];
-        
-
-		print("Nama\t:");
-		nama = in.nextLine(); 
-		// nama = in.nextLine();
-		
-		print("NIM\t:");
-		nim = in.next();
-
-        print("Masukkan nilai\n");
-        print("Nilai UTS = ");
-        nilaiUTS = in.nextInt();
-        print("Nilai UAS = ");
-        nilaiUAS = in.nextInt();
-
-		for(int a = 0; a < nilai.length; a++) {
-			print("Nilai tugas "+(a+1)+ " : ");
-			nilai[a] = in.nextInt();
-		}
-
-		print("\n=====================================\n");
-		
-		System.out.println("Nama\t: " +nama);
-		System.out.println("NIM\t: " +nim);
-		
-		int kkm = 75;
-		double sum = 0;
-		print("\n");
-		for(int a = 0; a < nilai.length; a++) {
-			System.out.println("Nilai\t: " +nilai[a]);
-			print("Indeks\t: ");
-			indeks(nilai[a]);
-			sum += nilai[a];
-		}
-		print("=====================================\n");
-		print("Rata rata = "+sum/nilai.length);
-		int min, max;
-		
-		min = nilai[0];
-		max = nilai[0];
-		
-		for(int a = 0; a < nilai.length; a ++) {
-				if(max < nilai[a]) {
-					max = nilai[a];
-				}if(min > nilai[a]) {
-					min = nilai[a];
-				}
-			}
-		System.out.println("\nNilai Maksimum\t:" + max);
-		System.out.println("Nilai Minimum\t:" + min);
-		
-		boolean or = true;
-		for(int a = 0; a < nilai.length; a ++) {
-			if(nilai[a]>=kkm && nilai[a]<=100) {
-				or = true;
-			}else if(nilai[a]>=0 && nilai[a]<kkm) {
-				or = false;
-			}else
-				print("INVALID");
-		}
-		if(or == true) {
-			print("Anda dinyatakan LULUS");
-		}else if(or == false) {
-			print("Anda dinyatakan TIDAK LULUS");
-		}
-	}
-
-	static void indeks(int i) {
+    static Scanner in = new Scanner(System.in);
+    static void indeks(int i) {
 		if(i < 0 || i > 100) {
 			System.out.println("Maaf, input salah");
 		}else if(i >= 85) {
@@ -98,8 +23,93 @@ public class SmallProject {
 			System.out.println("E");
 		}
 	}
+    static void print(String data){
+        System.out.print(data);
+    
+    }
+    static double nilaiAkhir(double uas, double uts, double sumTugas, double sumUH){
+        return (0.4*uas) + (0.3*uts) + (0.15*sumTugas/2) + (0.15*sumUH/2);
+    }
+    public static void main(String[] args) {
+        System.out.println("RAPORT");
 
-	static void print(String data) {
-		System.out.print(data);
-	}
+        String nama, nim;
+        print("Nama\t: ");
+        nama = in.nextLine();
+        print("NIM\t: ");
+        nim = in.next();
+
+        print("Masukkan nilai" + "\n");
+
+        int sumTugas = 0;
+        int[]nilai = new int[6];
+        for(int a = 0; a < 2; a++){
+            print("Nilai tugas " + (a+1) + "\t: ");
+            nilai[a] = in.nextInt();
+            sumTugas+=nilai[a];
+        }
+        int sumUH = 0;
+        for(int a = 2; a < 4 ; a++){
+            print("Nilai ulangan harian " + (a-1) + "\t: ");
+            nilai[a] = in.nextInt();
+            sumUH += nilai[a];
+        }
+        int uas, uts;
+
+        print("Nilai UTS\t: ");
+        uts = in.nextInt();
+        print("Nilai UAS\t: ");
+        uas = in.nextInt();
+        
+        nilai[4]= uts;
+        nilai[5]= uas;
+
+        double sum = 0;
+        for(int a = 0; a < nilai.length; a++){
+           sum+=nilai[a];
+        }
+       
+        double na = nilaiAkhir(uas, uts, sumTugas, sumUH);
+
+        print("====================\n");
+        
+        boolean statusNilai = true;
+        for(int a = 0; a < nilai.length; a++){
+            if(nilai[a] < 0 || nilai[a] > 100){
+                statusNilai = false;
+            }
+        }
+        if(statusNilai == true){
+            print("Nama\t: " + nama);
+            print("\nNIM\t: " + nim);
+            print("\nDAFTAR NILAI\n");
+            for(int a = 0; a < 2; a++){
+                print("\nTugas " + (a+1) + "\t: " + nilai[a]);
+            }
+            for(int a = 2; a < 4 ; a++){
+                print("\nUH " + (a-1) + "\t: " + nilai[a]);
+            }
+                print("\nUTS\t: " + nilai[4]);
+
+                print("\nUAS\t: " + nilai[5]);
+                
+                print("\n==================");
+                print("\nJumlah nilai\t: " + sum);
+                print("\nNilai akhir\t: " + na);
+                print("\nIndeks\t\t: ");
+                indeks((int)na);
+            }
+        else{
+            print("Maaf, nilai tidak valid");
+        }
+        
+        if(statusNilai==true){
+            if(na >= 75 && na <= 100){
+                print("Anda dinyatakan LULUS");
+            }else{
+                print("Maaf anda TIDAK LULUS");
+                
+            }
+        }
+    }
 }
